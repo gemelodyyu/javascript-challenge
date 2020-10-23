7c22bc72f9221e06cb46b1bef6e4d87f2065a73f
gemelodyyu <yuge.chang0512@gmail.com>
2020-10-23 01:17:21 -0500

    update level 1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,57 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// import data 
+var table = d3.select("#ufo-table");
+var tableBody = table.select("tbody");
+
+function createDefault() {
+    tableData.forEach(data => {
+        row = tableBody.append("tr")
+        row.append("td").text(data.datetime);
+        row.append("td").text(data.city);
+        row.append("td").text(data.state);
+        row.append("td").text(data.country);
+        row.append("td").text(data.shape);
+        row.append("td").text(data.durationMinutes);
+        row.append("td").text(data.comments);
+    });
+}
+createDefault();
+
+// create event handlers and create function 
+var searchField = d3.select("#datetime");
+var searchButton = d3.select("#filter-btn");
+var resetButton = d3.select("#reset-btn");
+
+resetButton.on("click", () => {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+
+    tableBody.remove();
+    tableBody = table.append("tbody");
+    createDefault();
+});
+
+searchButton.on("click", () => {
+
+    d3.event.preventDefault();
+    
+    var searchFor = searchField.property("value");
+    console.log(searchFor);
+    tableBody.remove();
+    tableBody = table.append("tbody");
+
+    tableData.filter(d => d.datetime.trim() == searchFor.trim())
+        .forEach(data => {
+            var row = tableBody.append("tr");
+            row.append("td").text(data.datetime);
+            row.append("td").text(data.city);
+            row.append("td").text(data.state);
+            row.append("td").text(data.country);
+            row.append("td").text(data.shape);
+            row.append("td").text(data.durationMinutes);
+            row.append("td").text(data.comments);
+        });
+
+})
