9088c73dec8e881ee67ba4e08c96167018795593
gemelodyyu <yuge.chang0512@gmail.com>
2020-10-26 18:27:02 -0500

    update level 2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -19,39 +19,65 @@ function createDefault() {
 }
 createDefault();
 
-// create event handlers and create function 
-var searchDate = d3.select("#datetime");
+// select buttons and input field
+var searchField = d3.select("#searchForm");
 var searchButton = d3.select("#filter-btn");
 var resetButton = d3.select("#reset-btn");
 
-resetButton.on("click", () => {
+// create event handlers 
+searchButton.on("click", runEnter); 
+searchField.on("submit", runEnter);
+resetButton.on("click", resetAll); 
+
+// functions 
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    
+    // filter by datetime 
+    var inputElementDate = d3.select("#datetime");
+    var searchForDate = inputElementDate.property("value");
+
+    var inputElementCity = d3.select("#city");
+    var searchForCity = inputElementCity.property("value");
+
+    var inputElementState = d3.select("#state");
+    var searchForState = inputElementState.property("value");
+
+    var inputElementCountry = d3.select("#country");
+    var searchForCountry = inputElementCountry.property("value");
+
+    var inputElementShape = d3.select("#shape");
+    var searchForShape = inputElementShape.property("value");
+
+    tableBody.remove();
+    tableBody = table.append("tbody");
+
+    var filteredData = tableData.filter(d => 
+                d.datetime.trim() === searchForDate.trim() ||
+                d.city.trim() === searchForCity.trim() ||
+                d.state.trim() === searchForState.trim() ||
+                d.country.trim() === searchForCountry.trim() ||
+                d.shape.trim() === searchForShape.trim()
+            ); 
+
+    filteredData.forEach(data => {
+        var row = tableBody.append("tr");
+        row.append("td").text(data.datetime);
+        row.append("td").text(data.city);
+        row.append("td").text(data.state);
+        row.append("td").text(data.country);
+        row.append("td").text(data.shape);
+        row.append("td").text(data.durationMinutes);
+        row.append("td").text(data.comments);
+    });
+}; 
+
+function resetAll() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
 
     tableBody.remove();
     tableBody = table.append("tbody");
     createDefault();
-});
-
-searchButton.on("click", () => {
-
-    d3.event.preventDefault();
-    
-    var searchFor = searchField.property("value");
-    console.log(searchFor);
-    tableBody.remove();
-    tableBody = table.append("tbody");
-
-    tableData.filter(d => d.datetime.trim() == searchFor.trim())
-        .forEach(data => {
-            var row = tableBody.append("tr");
-            row.append("td").text(data.datetime);
-            row.append("td").text(data.city);
-            row.append("td").text(data.state);
-            row.append("td").text(data.country);
-            row.append("td").text(data.shape);
-            row.append("td").text(data.durationMinutes);
-            row.append("td").text(data.comments);
-        });
-
-})
+}; 
