a315984eb79f3203d3e8c79bdb8293fb336e4357
gemelodyyu <yuge.chang0512@gmail.com>
2020-10-26 01:18:42 -0500

    update level2

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,21 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// import data 
+var table = d3.select("#ufo-table");
+var tableBody = table.select("tbody");
+
+function createDefault() {
+    tableData.forEach(data => {
+        row = tableBody.append("tr")
+        row.append("td").text(data.datetime);
+        row.append("td").text(data.city);
+        row.append("td").text(data.state);
+        row.append("td").text(data.country);
+        row.append("td").text(data.shape);
+        row.append("td").text(data.durationMinutes);
+        row.append("td").text(data.comments);
+    });
+}
+createDefault();
+
